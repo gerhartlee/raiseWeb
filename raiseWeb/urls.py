b6763e43c2d22ff7cd6a159e5b4373b0b1833617
gerhartlee <gerhart.lee@gmail.com>
2025-01-16 17:47:29 +0800

    v0.4

--- raiseWeb/urls.py
+++ raiseWeb/urls.py
@@ -21,8 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('detail/<int:id>', views.detail, name = 'detail-url'),
     path('', views.index),
     path('admin/', admin.site.urls),
+    path('delpost/<int:pid>/<str:del_pass>/', views.delpost),
+    path('list/', views.listing),
+    path('post/', views.posting),
+    path('contact/', views.contact),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
